d96ef489b634639f577ab6a0d0682e61a6970a77
Arthur123ms <arthurmagalhaesroney@gmail.com>
2026-05-12 20:09:07 -0300

    feat update codes resolve imports Button

--- src/components/ContactForm/ConstactForm.jsx
+++ src/components/ContactForm/ConstactForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useContext } from 'react'
 
 import './ContactForm.css'
 
-import Button from '/Users/Arthur Magalahães/REACT.Js/dnc-arq-arthur/src/components/Button/Button'
+import  Button  from "../Button/Button"
 
 import { AppContext } from '../../contexts/AppContext'
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import "../Header/Header.css"
 import Logo from '../../assets/dnc-logo 2.svg'
 
 // components
-import Button from '/Users/Arthur Magalahães/REACT.Js/dnc-arq-arthur/src/components/Button/Button'
+import  Button  from "../Button/Button"
 
 import { AppContext } from "../../contexts/AppContext"
 
